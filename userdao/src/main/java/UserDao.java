129692b5a1b8e4986d6cb2a48736ba108e3d566b
purechild <a01056490114@gmail.com>
2018-03-23 16:26:11 +0900

    delete some comments

--- userdao/src/main/java/UserDao.java
+++ userdao/src/main/java/UserDao.java
@@ -61,17 +61,7 @@ public class UserDao {
         return id;
     }
 
-    //개발자는 죽을 때까지 반복을 싫어해야 한다. copy & paste로 코딩 후 Test 수행 후 반복되는 동작을 Refactoring. 인텔리제이는 같은 부분을 알아서 다 바꿔줘서 좋음
-    //Refactor → Extract → Method : Ctrl + Alt + M
-    /*
-    요구사항이 들어왔는데 대부분 같고 하나의 메소드가 다를 때
-    부분적으로 다르지만 어떻게 다른지 모르면 추상화! → 메소드가 abstract면 클래스도 abstract
-    Template Method Pattern = 상위 클래스에서 처리의 흐름을 제어하며, 하위 클래스에서 처리의 내용을 구체화
-    Fatory Method Pattern = 인스턴스를 만드는 방법을 상위 클래스 측에서 결정하되, 구체적인 내용은 모두 하위 클래스 측에서 수행
-    */
     public Connection getConnection() throws ClassNotFoundException, SQLException {
-        //mysql driver load
-
         return connectionMaker.getConnection();
     }
 }
