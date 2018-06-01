ce89bfd4daa24cb40b763294d88cffac218efb82
purechild <a01056490114@gmail.com>
2018-06-01 15:28:02 +0900

    project base source

---
+++ project_practice/src/main/java/com/example/project_practice/ProjectPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.example.project_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPracticeApplication.class, args);
+	}
+}
---
+++ project_practice/src/test/java/com/example/project_practice/ProjectPracticeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.project_practice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectPracticeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
